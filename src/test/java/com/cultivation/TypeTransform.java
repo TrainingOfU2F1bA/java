1bc3dd3d933bb2089518ab82f5288af53972959c
Kuan Xin <kxin@thoughtworks.com>
2018-09-04 23:33:49 +0800

    [StringTest]:type transform exercise

---
+++ src/test/java/com/cultivation/TypeTransform.java
@@ -0,0 +1,21 @@
+package com.cultivation;
+
+import org.junit.jupiter.api.Test;
+
+public class TypeTransform {
+    {
+        byte init=123;
+        byte b=init;
+        short s=init;
+        int i=init;
+        long l=init;
+        float f=init;
+        double d=init;
+
+    }
+
+    @Test
+    void name() {
+
+    }
+}
