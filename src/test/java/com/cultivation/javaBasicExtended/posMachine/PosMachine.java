280b6712ea3575d1c4254fb6a14f9dec6d55115d
Kuan Xin <kxin@thoughtworks.com>
2018-09-09 22:02:11 +0800

    test[PosMachineTest]:should print receipt with multiple grouped items

--- src/test/java/com/cultivation/javaBasicExtended/posMachine/PosMachine.java
+++ src/test/java/com/cultivation/javaBasicExtended/posMachine/PosMachine.java
@@ -1,17 +1,29 @@
 package com.cultivation.javaBasicExtended.posMachine;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.core.FormatSchema;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SuppressWarnings({"WeakerAccess", "unused", "RedundantThrows"})
 public class PosMachine {
+
+    private Map<String, Product> collect;
+
     public void readDataSource(Reader reader) throws IOException {
         // TODO: please implement the following method to pass the test
         // <--start
-
+        ObjectMapper mapper = new ObjectMapper();
+        collect = mapper.<List<Product>>readValue(reader, new TypeReference<List<Product>>() { }) .stream()
+                .collect(Collectors.toMap(Product::getId, product -> product));
         // --end-->
     }
 
@@ -19,12 +31,34 @@ public class PosMachine {
         // TODO: please implement the following method to pass the test
         // <--start
         String line = System.lineSeparator();
-        final String expected =
-                "Receipts" + line +
-                        "------------------------------------------------------------" + line +
-                        "------------------------------------------------------------" + line +
-                        "Price: 0" + line;
-        return expected;
+        StringBuilder receipt = new StringBuilder("Receipts" + line + "------------------------------------------------------------" + line);
+        int amountPrice = 0;
+        if (barcodeContent != null && !barcodeContent.equals("[]")) {
+            String[] strings = barcodeContent.replaceAll("\"|\\[|\\]", "").split(",");
+            HashMap<String, Integer> amountMap = new HashMap<>();
+            ArrayList<String> receiptList = new ArrayList<>();
+            for (String productId:strings) {
+                Product product = collect.get(productId);
+                if (product.getId().equals(productId)) {
+                    amountMap.compute(productId,(k,v)->{
+                        if (v == null) return product.getPrice();
+                        else return v+product.getPrice();
+                    });
+                }
+            }
+            String[] productIdArray = Arrays.stream(strings).distinct().toArray(String[]::new);
+            for (String productId:productIdArray) {
+                String name = collect.get(productId).getName();
+                amountPrice += amountMap.get(productId);
+                Integer singlePrice = collect.get(productId).getPrice();
+                receipt.append(String.format("%s                      %d          %d\n"
+                        , name.length() < 10?(name+" "):name
+                        , singlePrice
+                        , amountMap.get(productId)/ singlePrice));
+            }
+        }
+        receipt.append("------------------------------------------------------------" + line + "Price: "+amountPrice + line);
+        return receipt.toString();
         // --end-->
     }
 }
