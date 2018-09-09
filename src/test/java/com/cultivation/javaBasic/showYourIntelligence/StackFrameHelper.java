783506d5ce469e807838f7acae0998c714f913f6
Kuan Xin <kxin@thoughtworks.com>
2018-09-09 12:56:49 +0800

    test[ExceptionTest]:should get method name in stack frame

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/StackFrameHelper.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/StackFrameHelper.java
@@ -6,7 +6,11 @@ public class StackFrameHelper {
     public static String getCurrentMethodName() {
         // TODO: please modify the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        try {
+            throw new NotImplementedException();
+        } catch (NotImplementedException e) {
+            return String.format("%s.%s",e.getStackTrace()[1].getClassName(),e.getStackTrace()[1].getMethodName());
+        }
         // --end-->
     }
 }
